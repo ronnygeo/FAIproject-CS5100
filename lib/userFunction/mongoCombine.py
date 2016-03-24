0de74dd6d1d4f7aa128e5dfb1c3f9f369f65f826
ronnygeo <ronnygeomatt@gmail.com>
2016-03-24 00:00:49 -0400

    Combine data and create new collection.

---
+++ lib/userFunction/mongoCombine.py
@@ -0,0 +1,28 @@
+from pymongo import MongoClient
+from bson.code import Code
+from mongoOperations import *
+
+client = MongoClient()
+
+db = client['yelp']
+user_collection = db.user
+review_collection = db.review
+business_collection = db.business
+
+db.create_collection('user_review')
+
+users = []
+i = 1
+for user in user_collection.find():
+	print i
+	user['reviews'] = review_collection.find({'user_id': user['user_id']})
+	r = 1
+	for review in user['reviews']:
+		print r
+		review['business_data'] = findBusiness([review['business_id']])
+		r += 1
+	users.append(user)
+	i += 1
+
+result = db.user_review.insert_many(users)
+print result.inserted_ids
